9aa03c98699e46af5e1fa2605dd4aeb27dfc8051
leoda20 <65856875+leoda20@users.noreply.github.com>
2025-07-01 20:02:14 +0100

    v3.9.6

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ $(function () {
 
     swup
 
-    ***************************/
+    *************************
     const options = {
         containers: ['#swupMain', '#swupMenu'],
         animateHistoryBrowsing: true,
@@ -33,6 +33,8 @@ $(function () {
         animationSelector: '[class="mil-main-transition"]'
     };
     const swup = new Swup(options);
+
+    **/
 
     /***************************
 
