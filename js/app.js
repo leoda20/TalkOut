4956b8ef268fcbea428982c14c0b10a92157fa1c
leoda20 <65856875+leoda20@users.noreply.github.com>
2025-12-03 22:35:08 +0000

    text

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = env.REACT_APP_SUPABASE_URL
-const SUPABASE_KEY = env.REACT_APP_ANON_KEY
+const SUPABASE_URL = 'https://ovomjuqotkdxcselclqp.supabase.co';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im92b21qdXFvdGtkeGNzZWxjbHFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ2OTU3MDYsImV4cCI6MjA4MDI3MTcwNn0.w35Nc9oe7rgYCyu4Obomno8WgrD4zIUf4fk5p-QY3Uo';
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 // Mount the ready-made Auth UI
